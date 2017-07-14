b7fe89853054c23287ecea8c989c738bb6ab3dee
Anthony Carlos <anthony@carlosfamily.com>
2017-07-13 22:47:29 -0400

    Initial attempt seems to work.

--- lib/file_util.rb
+++ lib/file_util.rb
@@ -2,10 +2,10 @@ require 'fileutils'
 
 class FileUtil
 
-  def self.move_file(existing_filename, new_filename)
+  def self.move_file(existing_filename, new_filename, noop: nil, verbose: true)
     return false if existing_filename == new_filename
     return false unless FileTest.exist?(existing_filename)
-    FileUtils.mv(existing_filename, new_filename)
+    FileUtils.mv(existing_filename, new_filename, noop: noop, verbose: verbose)
   end
 
 end
